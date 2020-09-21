6fc54824d75c274fcd8cd31de1318b583d72177a
JoaoLuizST <joaoluizseverot@gmail.com>
2020-09-20 22:46:34 -0300

    Move files
    Moving to new repo

---
+++ Camera1/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Camera1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_capturar = new System.Windows.Forms.Button();
+            this.btn_salvar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cb_dispositivos = new System.Windows.Forms.ComboBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.btn_fechar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(345, 284);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btn_capturar
+            // 
+            this.btn_capturar.Location = new System.Drawing.Point(366, 12);
+            this.btn_capturar.Name = "btn_capturar";
+            this.btn_capturar.Size = new System.Drawing.Size(154, 30);
+            this.btn_capturar.TabIndex = 1;
+            this.btn_capturar.Text = "Iniciar";
+            this.btn_capturar.UseVisualStyleBackColor = true;
+            this.btn_capturar.Click += new System.EventHandler(this.btn_capturar_Click);
+            // 
+            // btn_salvar
+            // 
+            this.btn_salvar.Location = new System.Drawing.Point(366, 48);
+            this.btn_salvar.Name = "btn_salvar";
+            this.btn_salvar.Size = new System.Drawing.Size(154, 30);
+            this.btn_salvar.TabIndex = 2;
+            this.btn_salvar.Text = "Salvar";
+            this.btn_salvar.UseVisualStyleBackColor = true;
+            this.btn_salvar.Click += new System.EventHandler(this.btn_salvar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(363, 259);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Dispositivos";
+            // 
+            // cb_dispositivos
+            // 
+            this.cb_dispositivos.FormattingEnabled = true;
+            this.cb_dispositivos.Location = new System.Drawing.Point(363, 275);
+            this.cb_dispositivos.Name = "cb_dispositivos";
+            this.cb_dispositivos.Size = new System.Drawing.Size(157, 21);
+            this.cb_dispositivos.TabIndex = 4;
+            // 
+            // btn_fechar
+            // 
+            this.btn_fechar.Location = new System.Drawing.Point(366, 84);
+            this.btn_fechar.Name = "btn_fechar";
+            this.btn_fechar.Size = new System.Drawing.Size(154, 30);
+            this.btn_fechar.TabIndex = 5;
+            this.btn_fechar.Text = "Fechar";
+            this.btn_fechar.UseVisualStyleBackColor = true;
+            this.btn_fechar.Click += new System.EventHandler(this.btn_fechar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 307);
+            this.Controls.Add(this.btn_fechar);
+            this.Controls.Add(this.cb_dispositivos);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_salvar);
+            this.Controls.Add(this.btn_capturar);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Câmera";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_capturar;
+        private System.Windows.Forms.Button btn_salvar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cb_dispositivos;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button btn_fechar;
+    }
+}
+
---
+++ Camera1/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace Camera1
+{
+    public partial class Form1 : Form
+    {
+        private bool DeviceExist = false;
+        private FilterInfoCollection videoDevices;
+        private VideoCaptureDevice videoSource = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void video_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap img = (Bitmap)eventArgs.Frame.Clone();
+            pictureBox1.Image = img;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btn_salvar.Enabled = false;
+            try
+            {
+                videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+                cb_dispositivos.Items.Clear();
+                if (videoDevices.Count == 0)
+                    throw new ApplicationException();
+
+                DeviceExist = true;
+                foreach (FilterInfo device in videoDevices)
+                {
+                    cb_dispositivos.Items.Add(device.Name);
+                }
+                cb_dispositivos.SelectedIndex = 0; //make dafault to first cam
+            }
+            catch (ApplicationException)
+            {
+                DeviceExist = false;
+                cb_dispositivos.Items.Add("Nenhum dispositivo encontrado!");
+            }
+        }
+
+        private void btn_capturar_Click(object sender, EventArgs e)
+        {
+            if (DeviceExist)
+            {
+                if(btn_capturar.Text == "Iniciar")
+                {
+                    videoSource = new VideoCaptureDevice(videoDevices[cb_dispositivos.SelectedIndex].MonikerString);
+                    videoSource.NewFrame += new NewFrameEventHandler(video_NewFrame);
+                    videoSource.Start();
+                    btn_capturar.Text = "Parar";
+                    btn_salvar.Enabled = false;
+                }
+                else
+                {
+                    videoSource.SignalToStop();
+                    videoSource = null;
+                    //pictureBox1.Image = null;
+                    btn_capturar.Text = "Iniciar";
+                    btn_salvar.Enabled = true;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Nenhum dispositivo encontrado!");
+            }
+        }
+
+        private void btn_fechar_Click(object sender, EventArgs e)
+        {
+            if (videoSource != null)
+            {
+                videoSource.SignalToStop();
+                videoSource = null;
+            }
+            this.Close();
+        }
+
+        private void btn_salvar_Click(object sender, EventArgs e)
+        {   
+            saveFileDialog1.Filter = "JPEG (*.jpg;*.jpeg;*jpeg;*.jfif)|*.jpg;*.jpeg;*jpeg;*.jfif";
+            DialogResult res = saveFileDialog1.ShowDialog();
+            if (res == DialogResult.OK)
+            {
+                pictureBox1.Image.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+            }
+        }
+    }
+}
